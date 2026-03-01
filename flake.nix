5f3d7e9c04cbcef6857cef75704c2a6c1dd4d021
diannaaa <diannaaav@gmail.com>
2026-03-01 23:56:14 +0800

    chore: setup nix development environment

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "NexusOS - Axle microkernel development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+
+        rustToolchain = pkgs.rust-bin.nightly.latest.default.override {
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+            "llvm-tools"
+            "clippy"
+            "rustfmt"
+            "miri"
+          ];
+          targets = [
+            "x86_64-unknown-none"
+          ];
+        };
+
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Rust
+            rustToolchain
+
+            # Testing
+            cargo-nextest
+            cargo-fuzz
+            cargo-mutants
+            cargo-llvm-cov
+            cargo-watch
+
+            # Build
+            mold
+
+            # Virtualization
+            qemu
+
+            # Debug
+            gdb
+
+            # Boot (Limine ISO)
+            limine
+            xorriso
+          ];
+
+          shellHook = ''
+            export DIRENV_LOG_FORMAT=""
+            echo "NexusOS dev environment loaded"
+            echo "Rust: $(rustc --version)"
+          '';
+        };
+      }
+    );
+}
